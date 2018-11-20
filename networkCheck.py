030244c4942d34bfb351922a121723fcc2131fca
Cody <bigbrocode@yahoo.com>
2018-11-20 10:54:29 -0500

    Worked with scaling graph

--- networkCheck.py
+++ networkCheck.py
@@ -3,7 +3,7 @@ import random
 chartData = []
 
 for i in range(10):
-    chartData.append(random.randint(0,400))
+    chartData.append(random.randint(0,1750))
 
 def update():
     del(chartData[0])
