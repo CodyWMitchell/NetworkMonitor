499fcaa174ac7713a6a19a9127febea8b2e83b42
Cody <bigbrocode@yahoo.com>
2018-11-21 09:04:35 -0500

    Added Wifi Name Checker

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
 from flask import Flask
 from flask import render_template
 from networkCheck import *
-import random
+import random, subprocess
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     update()
-    return render_template("index.html", data=chartData, color1=random.choice(["#E24E42","#E9B000","#EB6E80","#008F95"]), color2="#000", color3="white", networkName="Resnet") # Render the site with the current data
+
+    try:
+        sp = subprocess.check_output('iwgetid')
+        wifiName = sp.split('"')[1]
+    except:
+        wifiName = "Not Connected"
+
+    return render_template("index.html", data=chartData, color1=random.choice(["#E24E42","#E9B000","#EB6E80","#008F95"]), color2="#000", color3="white", networkName=wifiName) # Render the site with the current data
 
 if __name__ == '__main__':
     app.secret_key="Secret key"
