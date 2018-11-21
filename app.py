786ab5e72d1dc3018b64e0a10b60d66e09616ccf
Cody <bigbrocode@yahoo.com>
2018-11-21 09:07:28 -0500

    Reformatting app.py args

--- app.py
+++ app.py
@@ -14,7 +14,14 @@ def hello_world():
     except:
         wifiName = "Not Connected"
 
-    return render_template("index.html", data=chartData, color1=random.choice(["#E24E42","#E9B000","#EB6E80","#008F95"]), color2="#000", color3="white", networkName=wifiName) # Render the site with the current data
+    return render_template(
+    "index.html",
+    data=chartData,
+    color1=random.choice(["#E24E42","#E9B000","#EB6E80","#008F95"]),
+    color2="#000",
+    color3="white",
+    networkName=wifiName
+    ) # Render the site with the current data
 
 if __name__ == '__main__':
     app.secret_key="Secret key"
